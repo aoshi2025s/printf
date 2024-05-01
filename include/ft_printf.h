df5da913bccf5bbe8a9bd739de8ba3b42f3ad554
aoshi2025s <aoshi2025s@gmail.com>
2024-05-01 10:01:05 +0900

    header little change

--- include/ft_printf.h
+++ include/ft_printf.h
@@ -6,8 +6,16 @@
 /*   By: yoaoki <yoaoki@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 02:42:02 by yoaoki            #+#    #+#             */
-/*   Updated: 2024/05/01 02:42:07 by yoaoki           ###   ########.fr       */
+/*   Updated: 2024/05/01 10:00:24 by yoaoki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
+# include "libft.h"
+# include <stdarg.h>
+
+int	ft_printf(const char *s, ...);
+
+#endif
